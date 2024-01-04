f3770706eba9943df9195a14db11b7588e0a1374
LeHoangAnh021203 <hoanganhle0203@gmail.com>
2024-01-04 20:16:26 +0700

    style for quantityElement, create function priceElement and totalElement

--- script.js
+++ script.js
@@ -33,6 +33,11 @@ function quantityElement(quantityNumber) {
   const plusBtn = document.createElement("button");
   const quantity = document.createElement("span");
 
+  quantityControlWrapper.classList.add("quantity-control-wrapper")
+  minusBtn.classList.add("minus-button");
+  plusBtn.classList.add("plus-button");
+  quantity.classList.add("quantity");
+
   quantity.textContent = quantityNumber;
   minusBtn.innerHTML = "-";
   plusBtn.innerHTML = "+";
@@ -42,10 +47,25 @@ function quantityElement(quantityNumber) {
   return quantityControlWrapper;
 }
 
+function priceElement(price) {
+  const priceElement = document.createElement("span");
+  const value = document.createElement("span");
+  const Price = document.createElement("span");
+
+  Price.textContent = price;
+  value.innerHTML = "$";
+
+  priceElement.appendChild(value);
+  priceElement.appendChild(Price);
+
+  return priceElement;
+}
+
 function handleGetData() {
   const cartItems = cartData.cartItems;
+  let totalValue = 0;
 
-  // Đảm bảo tồn tại ít nhất một phần tử trong mảng cartItems
+  
   if (cartItems?.length) {
     const fragment = new DocumentFragment();
     const itemArea = document.querySelector(".item-area");
@@ -56,18 +76,41 @@ function handleGetData() {
       const span = document.createElement("span");
       const image = document.createElement("img");
       const quantity = quantityElement(item.quantity);
+      const priceEl = priceElement(item.price);
+      const cancel = document.createElement("span");
+      const hrElement = document.createElement("hr");
+
+      cancel.innerHTML = "X"
       image.src = item.image;
-      image.style.width = "30%";
+      image.style.width = "15%";
 
       divElement.classList.add("item-wrapper");
-
+      hrElement.classList.add("hr-element");
+      
+      
       divElement.appendChild(image);
       span.textContent = item.itemName;
       divElement.appendChild(span);
       divElement.appendChild(quantity);
+      divElement.appendChild(priceEl);
+      divElement.appendChild(cancel);
       fragment.appendChild(divElement);
+      fragment.appendChild(hrElement);
+      
+      totalValue += parseInt(item.price) * item.quantity;
+      // totalElement.textContent = "Subtotal $" + totalValue;
     });
+    const subElement = document.createElement("span");
+    const totalElement = document.createElement("span");
+
+    subElement.textContent = "Subtotal";
+    totalElement.textContent = `$${totalValue}`;
     itemArea.appendChild(fragment);
+    itemArea.appendChild(subElement);
+    itemArea.appendChild(totalElement);
+
+    subElement.classList.add("sub-element");
+    totalElement.classList.add("total-element");
   } else {
     console.error("Mảng cartItems rỗng.");
   }
